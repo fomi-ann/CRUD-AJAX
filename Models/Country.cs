f07bce4a37a6aa22ea4fb65ef46f72a6b6b9f7cb
Anna Fomina <ann.fomi77@gmail.com>
2025-05-14 16:11:13 +0300

    Update Country.cs
    Disable nullable

--- Models/Country.cs
+++ Models/Country.cs
@@ -4,6 +4,15 @@ namespace CRUD_AJAX.Models
 {
     public class Country
     {
-        
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(3)]
+        public string Code { get; set; }
+
+        [Required]
+        [MaxLength(75)]
+        public string CurrencyName { get; set; } = "";
     }
 }
