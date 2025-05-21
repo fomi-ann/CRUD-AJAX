44217de9aa83e72b16cc95c989e3c4b104080f91
Anna Fomina <ann.fomi77@gmail.com>
2025-05-21 09:21:38 +0300

    Add Create country modal for Create customer view
    Error: Save btn doesn't close the modal window

--- Controllers/CountryController.cs
+++ Controllers/CountryController.cs
@@ -36,6 +36,21 @@ namespace CRUD_AJAX.Controllers
         }
 
         [HttpGet]
+        public IActionResult CreateModalForm()
+        {
+            Country country = new Country();
+            return PartialView("_CreateModalForm", country);
+        }
+
+        [HttpPost]
+        public IActionResult CreateModalForm(Country country)
+        {
+            _context.Add(country);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpGet]
         public IActionResult Details(int Id)
         {
             Country country = GetCountry(Id);
@@ -92,6 +107,26 @@ namespace CRUD_AJAX.Controllers
             }
 
             return RedirectToAction(nameof(Index));
+        }
+
+        public JsonResult GetCountries()
+        {
+            var lstCountries = new List<SelectListItem>();
+            List<Country> Countries = _context.Countries.ToList();
+            lstCountries = Countries.Select(ct => new SelectListItem()
+            {
+                Value = ct.Id.ToString(),
+                Text = ct.Name
+            }).ToList();
+
+            var defItem = new SelectListItem()
+            {
+                Value = "",
+                Text = "----Select Country----"
+            };
+
+            lstCountries.Insert(0, defItem);
+            return Json(lstCountries);
         }
 
     }
